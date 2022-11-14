930603d539c2906d714b787ebf1a1612229dd920
Ahmed <morsyahmed402@gmail.com>
2022-11-14 15:44:17 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: [
-      "wildfiresocial.com",
-      "upload.wikimedia.org",
-      "images.pexels.com",
-      "fakestoreapi.com",
-    ],
-  },
 };
 
 module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from "next/head";
-import Banner from "../components/Banner";
-import Feed from "../components/Feed";
-import Header from "../components/Header";
 
-export default function Home({ products }) {
+export default function Home() {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Head>
@@ -11,21 +8,7 @@ export default function Home({ products }) {
         <meta name="description" content="E-commerce" />
         <link rel="icon" href="/favicon.ico" className="rounded-full" />
       </Head>
-      <Header />
-      <main className="max-w-5xl mx-auto">
-        <Banner />
-        <Feed products={products} />
-      </main>
+      <main>Hey</main>
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const products = await fetch("https://fakestoreapi.com/products").then(
-    (res) => res.json()
-  );
-
-  return {
-    props: { products },
-  };
 }
